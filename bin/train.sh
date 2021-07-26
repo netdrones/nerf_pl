87e93363765e53c4cdb9b9c29e44031d4c728c70
Lucas Hahn <lhahn1@me.com>
2021-07-26 12:36:31 -0400

    Add directory checking to train.sh

--- bin/train.sh
+++ bin/train.sh
@@ -6,17 +6,27 @@ DOWNSCALE=$3
 
 NUM_GPUS="$(nvidia-smi --query_gpu=name --format=csv,nohearder | wc -l)"
 
-if [ ! -d "$WORKSPACE_DIR/dense" ]; then sh +x bin/run_colmap.sh $WORKSPACE_DIR; fi
+if [ ! -d "$WORKSPACE_DIR/dense" ]
+then
+	if [ ! -d $WORKSPACE_DIR/images ]
+   	then
+     		mkdir -p $WORKSPACE_DIR/images
+     		mv $WORKSPACE_DIR/*.jpg $WORKSPACE_DIR/images 2> /dev/null
+     		mv $WORKSPACE_DIR/*.JPG $WORKSPACE_DIR/images 2> /dev/null
+     		mv $WORKSPACE_DIR/*.png $WORKSPACE_DIR/images 2> /dev/null
+   	fi
+   	sh +x bin/run_colmap.sh $WORKSPACE_DIR
+fi
 
 python generate_splits.py $WORKSPACE_DIR/dense/images $WORKSPACE_DIR/$WORKSPACE_DIR.tsv $WORKSPACE_DIR $WORKSPACE_DIR/database.db
 if [ ! -d "$WORKSPACE_DIR/cache" ]; then \
-  python prepare_phototourism.py --root_dir $WORKSPACE_DIR --img_downscale $DOWNSCALE; \
+	python prepare_phototourism.py --root_dir $WORKSPACE_DIR --img_downscale $DOWNSCALE; \
 fi
 
 python train.py \
-    --root_dir $WORKSPACE_DIR --dataset_name phototourism \
-    --img_downscale $DOWNSCALE --use_cache --N_importance 64 --N_samples 64 \
-    --encode_a --encode_t --beta_min 0.03 --N_vocab 1500 \
-    --num_epochs 20 --num_gpus $NUM_GPUS --batch_size 1024 \
-    --optimizer adam --lr 5e-4 --lr_scheduler cosine \
-    --exp_name $EXP_NAME
+	--root_dir $WORKSPACE_DIR --dataset_name phototourism \
+    	--img_downscale $DOWNSCALE --use_cache --N_importance 64 --N_samples 64 \
+    	--encode_a --encode_t --beta_min 0.03 --N_vocab 1500 \
+    	--num_epochs 20 --num_gpus $NUM_GPUS --batch_size 1024 \
+    	--optimizer adam --lr 5e-4 --lr_scheduler cosine \
+    	--exp_name $EXP_NAME
